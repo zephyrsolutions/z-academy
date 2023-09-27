9062465f2a06852eb600d7dc5a6bbb7a06e6b93f
Jyotirmoy <jyotirmoydas12@gmail.com>
2023-09-27 16:42:59 +0530

    Cleaned staging branch

--- routes/timeTable.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const timeTableController = require('../controllers/timeTable')
-
-router.get('/', timeTableController.getIndex)
-router.get('/slot', timeTableController.getSlot)
-router.post('/addSlot', timeTableController.addSlot)
-
-module.exports = router
